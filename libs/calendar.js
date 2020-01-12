5dde4c4b36195621bfb0c4495f77be8fef30e28c
jeromelebaron <lebaronjerome@free.fr>
2020-01-12 19:27:15 +0100

    Extract elements related to calendar in lib

---
+++ libs/calendar.js
@@ -0,0 +1,1 @@
+module.exports.runningName = 'Course a pied';
--- scripts/Rappel_affaire_de_course.js
+++ scripts/Rappel_affaire_de_course.js
@@ -1,9 +1,9 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: pink; icon-glyph: magic;
-const runningCalendarName = 'Course a pied';
-let runningCalendar = await Calendar.forEventsByTitle(runningCalendarName);
 const efCalendarEventName = 'EF';
+const CalendarLib = importModule('libs/calendar');
+let runningCalendar = await Calendar.forEventsByTitle(CalendarLib.runningName);
 let weekRunningCalendarEvents = await CalendarEvent.thisWeek([runningCalendar]);
 let efWeekRunningCalendarEvents = extractEfWeekRunningCalendarEvents(weekRunningCalendarEvents);
 
